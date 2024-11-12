7b85202c88ffbfc3be1e5407abda70bbdd3bed9a
Jamshed Alam <jamshedalam1777@gmail.com>
2024-11-13 00:33:13 +0530

    contact form issue resolve

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -6,8 +6,8 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, 
+  port: 465,
+  secure: true, 
   auth: {
     user: process.env.EMAIL_ADDRESS,
     pass: process.env.GMAIL_PASSKEY, 
